b1103c7161a0685b12f2ce6e9af52bb4f04dc353
Rodrigo Klaes <rodrigoklaes@gmail.com>
2023-03-08 19:47:13 -0300

    feat: add endpoints doc

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-require("./routes/index")(app);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+require('./routes/index')(app);
 
 app.listen(port, console.log(`App running at ${port}`));
