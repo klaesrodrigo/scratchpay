a83abc37e3ce706d8515d54c0281717a97d035d5
Rodrigo Klaes <rodrigoklaes@gmail.com>
2023-03-08 19:48:55 -0300

    feat: created routes for new endpoints

--- src/routes/clinics.js
+++ src/routes/clinics.js
@@ -1,8 +1,10 @@
-const { makeClinicController } = require("../factories/makeClinicController");
+const { makeClinicController } = require('../factories/makeClinicController');
 
-const routes = require("express").Router();
+const routes = require('express').Router();
 const clinicController = makeClinicController();
 
-routes.get("/", clinicController.list);
+routes.get('/', clinicController.list);
+routes.get('/vet', clinicController.listVet);
+routes.get('/dental', clinicController.listDental);
 
-module.exports = (app) => app.use("/clinics", routes);
+module.exports = (app) => app.use('/clinics', routes);
